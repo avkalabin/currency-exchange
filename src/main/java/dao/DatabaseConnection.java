4ea1847b88a317cac3b26de19fbc42f04b9fe054
69KalabinAV <KalabinAV@utgard.cbr.ru>
2026-01-22 21:08:44 +0500

    implement SQLite DB init from schema.sql

--- src/main/java/dao/DatabaseConnection.java
+++ src/main/java/dao/DatabaseConnection.java
@@ -1,8 +1,10 @@
 package dao;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,7 @@ public class DatabaseConnection {
     private static final String DB_URL = "jdbc:sqlite:currency_exchange.db";
     private static Connection connection = null;
     private static final Logger logger = LoggerFactory.getLogger(DatabaseConnection.class);
+    private static boolean isDbInitialized;
 
     private DatabaseConnection() {
     }
@@ -20,8 +23,12 @@ public class DatabaseConnection {
         try {
             if (connection == null || connection.isClosed()) {
                 connection = DriverManager.getConnection(DB_URL);
-                connection.createStatement().execute("PRAGMA foreign_keys = ON");
+                 connection.createStatement().execute("PRAGMA foreign_keys = ON");
                 logger.info("Подключение к SQLite успешно установлено");
+            }
+            if (!isDbInitialized) {
+                initDb();
+                isDbInitialized = true;
             }
             return connection;
         } catch (SQLException e) {
@@ -30,4 +37,30 @@ public class DatabaseConnection {
         }
     }
 
+    private static void initDb() {
+        String sql = readResource();
+        String[] statements = sql.split(";");
+        for (String s : statements) {
+            String trimmed = s.trim();
+            if (!trimmed.isEmpty()) {
+                try (Statement st = connection.createStatement()) {
+                    st.executeUpdate(trimmed);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private static String readResource() {
+        try (var in = DatabaseConnection.class.getClassLoader()
+                                              .getResourceAsStream("schema.sql")) {
+            if (in == null) {
+                throw new IOException("schema.sql not found");
+            }
+            return new String(in.readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
